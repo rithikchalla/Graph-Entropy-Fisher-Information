cf1536ce4a300dc9b48cb1817b0814a218a5fefb
rithik <19rithik@gmail.com>
2022-07-26 20:56:54 -0400

    Modified variable names

--- graph-entropy-fisher.py
+++ graph-entropy-fisher.py
@@ -14,23 +14,23 @@ def graph_entropy(adjacency_matrix):
         raise Exception("Input matrix should be a square matrix")
     
     # This will return the number of nodes in the graph
-    nodes = adjacency_matrix.shape[0]
+    no_of_nodes = adjacency_matrix.shape[0]
     
     # Computes graph entropy from (Equation 4 from reference article)
-    normalized_graph_entropy = (1/(nodes * np.log(nodes-1))) * (sum(np.log(np.sum(adjacency_matrix, axis=0))))
+    normalized_graph_entropy = (1/(no_of_nodes * np.log(no_of_nodes-1))) * (sum(np.log(np.sum(adjacency_matrix, axis=0))))
 
     # Generate random walker path from the adjacency matrix (Equation 1 from the reference article)
     random_walk_mat = np.zeros(adjacency_matrix.shape)
-    for i in range(nodes):
+    for i in range(no_of_nodes):
         random_walk_mat[i,] = adjacency_matrix[i,]/np.sum(adjacency_matrix[i,])
     
-    node_information = np.zeros((1, nodes))
+    node_information = np.zeros((1, no_of_nodes))
 
     # Compute normalized graph fisher information (Equation 5 and Equation 6 from the reference article)
-    for k in range(nodes):
-        list_values = list(map((lambda l: ((sqrt(random_walk_mat[k, l+1]) - sqrt(random_walk_mat[k, l])) ** 2) if k != l else 0), range(nodes-1)))
-        node_information[0, k] = (0.5 * (sum(list_values)))
+    for i in range(no_of_nodes):
+        list_values = list(map((lambda j: ((sqrt(random_walk_mat[i, j+1]) - sqrt(random_walk_mat[i, j])) ** 2) if i != j else 0), range(no_of_nodes-1)))
+        node_information[0, j] = (0.5 * (sum(list_values)))
 
-    normalized_graph_fisher_information = np.sum(node_information[0,], axis=0)/nodes
+    normalized_graph_fisher_information = np.sum(node_information[0,], axis=0)/no_of_nodes
 
     return (normalized_graph_entropy, normalized_graph_fisher_information)
